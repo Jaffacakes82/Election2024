969d63363436ee1adb18f680b4caf3a10d69524d
Joe Ellis <joe@jellis.tech>
2024-06-15 11:12:34 +0100

    Remove Https redirection as its interfering with cloudflare dns

--- Election2024.Web/Program.cs
+++ Election2024.Web/Program.cs
@@ -24,7 +24,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
